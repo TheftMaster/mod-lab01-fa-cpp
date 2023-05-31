a0dcbf27b2bc3543a01907f5a3e04b93c9e1dffe
Рамазан Чопсиев <chopsiev.ramazan@yandex.ru>
2023-05-31 21:49:57 +0300

    Версия 1.0

--- src/fun.cpp
+++ src/fun.cpp
@@ -1,14 +1,90 @@
 // Copyright 2022 UNN-IASR
+
 #include "fun.h"
+#include <cctype>
 
-unsigned int faStr1(const char *str) {
-    return 0;
+unsigned int faStr1(const char* str)
+{
+    unsigned int count = 0;
+    bool isWord = false;
+
+    while (*str != '\0')
+    {
+        if (std::isalpha(*str) && !std::isdigit(*str))
+        {
+            if (!isWord)
+            {
+                count++;
+                isWord = true;
+            }
+        }
+        else
+        {
+            isWord = false;
+        }
+
+        str++;
+    }
+
+    return count;
 }
 
-unsigned int faStr2(const char *str) {
-    return 0;
+unsigned int faStr2(const char* str)
+{
+    unsigned int count = 0;
+    bool isWord = false;
+
+    while (*str != '\0')
+    {
+        if (std::isupper(*str))
+        {
+            if (!isWord)
+            {
+                isWord = true;
+                count++;
+            }
+        }
+        else if (std::islower(*str) || std::isspace(*str))
+        {
+            isWord = false;
+        }
+
+        str++;
+    }
+
+    return count;
 }
 
-unsigned int faStr3(const char *str) {
-    return 0;
+unsigned int faStr3(const char* str)
+{
+    unsigned int count = 0;
+    unsigned int sum = 0;
+    bool isWord = false;
+
+    while (*str != '\0')
+    {
+        if (std::isalpha(*str))
+        {
+            if (!isWord)
+            {
+                isWord = true;
+                count++;
+            }
+
+            sum++;
+        }
+        else
+        {
+            isWord = false;
+        }
+
+        str++;
+    }
+
+    if (count == 0)
+    {
+        return 0;
+    }
+
+    return (sum + count - 1) / count;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,20 @@
 // Copyright 2022 UNN-IASR
-#include "fun.h"
 
-int main() {
+#include "fun.h"
+#include <iostream>
+
+int main()
+{
+    const char* str = "Hello World! 123 Testing 456";
+
+    unsigned int count1 = faStr1(str);
+    std::cout << "Number of words without digits: " << count1 << std::endl;
+
+    unsigned int count2 = faStr2(str);
+    std::cout << "Number of words starting with uppercase letter: " << count2 << std::endl;
+
+    unsigned int avgLength = faStr3(str);
+    std::cout << "Average word length: " << avgLength << std::endl;
+
     return 0;
 }
