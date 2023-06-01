51ffa9faa9f7c1bf204cf2c75d8b053833a353a6
TheftMaster <116089430+TheftMaster@users.noreply.github.com>
2023-06-01 21:23:03 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -3,7 +3,7 @@
 #include <cctype>
 #include "fun.h"
 
-unsigned int faStr1(const char* str) {
+unsigned int faStr1(const char str) {
     unsigned int count = 0;
     bool inWord = false;
 
@@ -23,7 +23,7 @@ unsigned int faStr1(const char* str) {
     return count;
 }
 
-unsigned int faStr2(const char* str) {
+unsigned int faStr2(const charstr) {
     unsigned int count = 0;
     bool inWord = false;
 
@@ -55,13 +55,13 @@ unsigned int faStr2(const char* str) {
     return count;
 }
 
-unsigned int faStr3(const char* str) {
+unsigned int faStr3(const char str) {
     unsigned int totalLength = 0;
     unsigned int wordCount = 0;
     bool inWord = false;
 
     while (str != '\0') {
-        if (std::isalpha(str)) {
+        if (std::isalpha(*str)) {
             if (!inWord) {
                 inWord = true;
                 wordCount++;
